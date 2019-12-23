1100e3d54fed161f942b7fc70c0c6cd12bd4a6ff
Thannoli <thannoli.kadhiroli@nestle.com>
2019-12-23 11:47:32 +0530

    Updates

---
+++ precache-manifest.b35d6a4fdaf95e2488596d1a430b136a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e88375228420b415dcc27a2e527311",
+    "url": "/newcase/index.html"
+  },
+  {
+    "revision": "1fb2be070ca3c521f720",
+    "url": "/newcase/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6595575290e27130781",
+    "url": "/newcase/static/js/2.112be090.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newcase/static/js/2.112be090.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fb2be070ca3c521f720",
+    "url": "/newcase/static/js/main.6702f369.chunk.js"
+  },
+  {
+    "revision": "8fa25cb3325d342200ed",
+    "url": "/newcase/static/js/runtime-main.062b9a2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newcase/static/media/logo.5d5d9eef.svg"
+  }
+]);
